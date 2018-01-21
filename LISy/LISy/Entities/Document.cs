e21ba0083a9af068e9f456d1c70b57eb5620da29
Prometheus <fcicrcm@gmail.com>
2018-01-21 17:18:44 +0300

    Updated Documents. Working on its children.

--- LISy/LISy/Entities/Document.cs
+++ LISy/LISy/Entities/Document.cs
@@ -10,9 +10,51 @@ namespace LISy.Entities
     /// Shouldn't be declared somewhere
     /// It is just an implementation of interface <code>IDocument</code>
     /// </summary>
-    public class Document: IDocument
+    public class Document//: IDocument // I'm not sure if there are abstract classes in C#, but if yes, Documents should be such one.
     {
-        public string Title { get; set; }
+        private string[] Authors { get; } // we should discuss setters, getters, publicity.
+
+        private string Title { get; }
+
+        private int Price { get; }
+
+        private string[] Keywords { get; }
+
+        //private LinkedList<Copy> Copies { get; }
+
+        public Document(string[] authors, string title, int price, string[] keys, int copy_amount)
+        {
+            Authors = authors;
+            Title = title;
+            Price = price;
+            Keywords = keys;
+        }
+
+        public void addCopy()
+        {
+
+        }
+
+        public void addCopies(int n)
+        {   
+            // check n > 1
+            for(int i = 1; i <= n; ++i) addCopy();
+        }
+
+        public void removeCopy()
+        {
+            // check if Copies.amount > 0. If yes, check if there are some untaken copies.
+        }
+
+        public void removeCopies(int n)
+        {
+            // check n > 1
+            for (int i = 1; i <= n; ++i) removeCopy();
+        }
+
+        // Reserved, not mine. (c)Svyat
+
+        /* public string Title { get; set; }
 
         public string Author { get; set; }
 
@@ -26,6 +68,6 @@ namespace LISy.Entities
             Author = author;
             Room = room;
             Level = level;
-        }
+        }*/
     }
 }
