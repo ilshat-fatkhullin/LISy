ad845739c4464a373449aba16924dc478bfa15e8
DESKTOP-6N60G2V\Aidar <aidarahmetshin56@gmail.com>
2018-03-01 21:08:25 +0300

    Долгожданный коммит!!!!!

---
+++ LISy/LISy/MyUserTable.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LISy
+{
+    class MyUserTable
+    {
+        public MyUserTable(int Id, string FirtsName, string LastName, string town, string street, int house, int flat, string phoneNumber, int countBookedBooks)
+        {
+            this.Id = Id;
+            
+        }
+        public int Id { get; set;}
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string town { get; set; }
+        public string street { get; set; }
+        public int house { get; set; }
+        public int flat { get; set; }
+        public int phoneNumber { get; set; }
+        public int countBookedBooks { get; set; }
+    }
+
+}
