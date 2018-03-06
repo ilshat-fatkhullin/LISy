a837385fce038db77b23b69372438384d91c01f6
Ilshat Fatkhullin <ilshat.fatkhullin@gmail.com>
2018-03-06 21:45:49 +0300

    no message

--- LISy/LISyTest/Integrated/Delivery2.cs
+++ LISy/LISyTest/Integrated/Delivery2.cs
@@ -22,7 +22,7 @@ namespace LISyTest.Integrated
                 "patron_1", "12345");
             LibrarianDataManager.AddUser(new Student("StudentName", "StudentSurname", "80000000000", "Address"),
                 "patron_2", "12345");
-            LibrarianDataManager.AddUser(new Student("StudentName", "StudentSurname", "80000000000", "Address"),
+            LibrarianDataManager.AddUser(new Student("Elvira", "Espindola", "30003", "Via del Corso, 22"),
                 "patron_3", "12345");
 
             LibrarianDataManager.AddDocument(new Book("Authors", "Book_1", "Publisher", "Edition", 2018, false, "Keys", "", 100));
@@ -30,19 +30,29 @@ namespace LISyTest.Integrated
             LibrarianDataManager.AddDocument(new Book("Authors", "Book_2", "Publisher", "Edition", 2018, false, "Keys", "", 100));
             LibrarianDataManager.AddCopy(2, new Copy(2, 1, 2));
             LibrarianDataManager.AddDocument(new Book("Authors", "Book_3", "Publisher", "Edition", 2018, false, "Keys", "", 100));
-            LibrarianDataManager.AddCopy(1, new Copy(3, 1, 2));
+            LibrarianDataManager.AddCopy(1, new Copy(3, 1, 3));
             LibrarianDataManager.AddDocument(new AVMaterial("Authors", "AVMaterial_1", "Keys", "", 100));
-            LibrarianDataManager.AddDocument(new AVMaterial("Authors", "AVMaterial_2", "Keys", "", 100));                       
+            LibrarianDataManager.AddCopy(1, new Copy(4, 1, 4));
+            LibrarianDataManager.AddDocument(new AVMaterial("Authors", "AVMaterial_2", "Keys", "", 100));
+            LibrarianDataManager.AddCopy(1, new Copy(5, 1, 5));            
 
             Assert.AreEqual(LibrarianDataManager.GetNumberOfCopies(), 8);
-            Assert.AreEqual(LibrarianDataManager.GetNumberOfUsers(), 8);
+            Assert.AreEqual(LibrarianDataManager.GetNumberOfUsers(), 4);
         }
 
         [TestMethod]
         public void TestCase2()
         {
             TestCase1();
+            LibrarianDataManager.DeleteCopy(new Copy(1, 1, 1));
+            LibrarianDataManager.DeleteCopy(new Copy(1, 1, 1));
+            LibrarianDataManager.DeleteCopy(new Copy(3, 1, 3));
             LibrarianDataManager.DeleteUser(new Student("StudentName", "StudentSurname", "80000000000", "Address"));
+
+            int n = LibrarianDataManager.GetNumberOfCopies();
+            int s = LibrarianDataManager.GetNumberOfUsers();
+            Assert.AreEqual(LibrarianDataManager.GetNumberOfCopies(), 5);
+            Assert.AreEqual(LibrarianDataManager.GetNumberOfUsers(), 3);
         }
 
         [TestMethod]
@@ -55,6 +65,10 @@ namespace LISyTest.Integrated
             Assert.AreEqual(faculty.Phone, "30001");
             Assert.AreEqual(faculty.Address, "ViaMargutta, 3");
             faculty = LibrarianDataManager.GetUserById(4);
+            Assert.AreEqual(faculty.FirstName, "Elvira");
+            Assert.AreEqual(faculty.SecondName, "Espindola");
+            Assert.AreEqual(faculty.Phone, "30003");
+            Assert.AreEqual(faculty.Address, "Via del Corso, 22");
         }
 
         [TestMethod]
