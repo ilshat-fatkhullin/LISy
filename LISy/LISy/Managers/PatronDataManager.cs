6e9ba8b361175ee7952fde129c365e268ea73b7a
Ilshat Fatkhullin <ilshat.fatkhullin@gmail.com>
2018-04-02 07:42:25 +0300

    Added notifications.

--- LISy/LISy/Managers/PatronDataManager.cs
+++ LISy/LISy/Managers/PatronDataManager.cs
@@ -1,4 +1,6 @@
-﻿using RestSharp;
+﻿using LISy.Entities.Notifications;
+using System;
+using System.Collections.Generic;
 
 namespace LISy.Managers
 {
@@ -26,6 +28,25 @@ namespace LISy.Managers
             {
                 DocumentId = documentId,
                 PatronId = patronId
+            });
+        }
+
+        public static Notification[] GetNotifications(long patronId)
+        {
+            var output = HttpHelper.MakeGetRequest<List<Notification>>("patron/get_notifications",
+                new Tuple<string, string>[] {
+                    new Tuple<string, string>("patronId", Convert.ToString(patronId))
+                });
+            if (output == null)
+                return new Notification[] { };
+            return output.ToArray();
+        }
+
+        public static void ReadNotification(long notificationId)
+        {
+            HttpHelper.MakePutRequest("patron/read_notification", new
+            {
+                Id = notificationId,                
             });
         }
     }
